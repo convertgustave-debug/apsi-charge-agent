e713ba2f743e59c6e1a4f15a9330a0c9d5d3435c
convertgustave-debug <convertgustave@gmail.com>
2026-03-08 16:17:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -286,29 +286,23 @@ def export_excel(df_detail, synthese_par_horizon):
         worksheet.add_chart(chart4, "B20")
 
         chart2 = BarChart()
-        chart2.title = "Comparaison des charges"
-        chart2.y_axis.title = "Points"
+        chart2.title = "Comparaison des taux de charge"
+        chart2.y_axis.title = "%"
         chart2.x_axis.title = "CDP"
+    
+        data1 = Reference(worksheet, min_col=3, min_row=1, max_row=worksheet.max_row-1)
+        data2 = Reference(worksheet, min_col=5, min_row=1, max_row=worksheet.max_row-1)
+        data3 = Reference(worksheet, min_col=7, min_row=1, max_row=worksheet.max_row-1)
 
-        data = Reference(
-            worksheet,
-            min_col=2,
-            max_col=6,
-            min_row=1,
-            max_row=worksheet.max_row-1
-        )
+        cats = Reference(worksheet, min_col=1, min_row=2, max_row=worksheet.max_row-1)
 
-        cats = Reference(
-            worksheet,
-            min_col=1,
-            min_row=2,
-            max_row=worksheet.max_row-1
-        )
+        chart2.add_data(data1, titles_from_data=True)
+        chart2.add_data(data2, titles_from_data=True)
+        chart2.add_data(data3, titles_from_data=True)
 
-        chart2.add_data(data, titles_from_data=True)
         chart2.set_categories(cats)
 
-        worksheet.add_chart(chart2, "J15")
+        worksheet.add_chart(chart2, "J16")
 
         surcharge_rows = []
 
@@ -346,7 +340,7 @@ def export_excel(df_detail, synthese_par_horizon):
             chart3.add_data(data, titles_from_data=False)
             chart3.set_categories(cats)
 
-            worksheet.add_chart(chart3, "J28")
+            worksheet.add_chart(chart3, "J29")
 
         green_fill = PatternFill(start_color="C6EFCE", end_color="C6EFCE", fill_type="solid")
         orange_fill = PatternFill(start_color="FFD966", end_color="FFD966", fill_type="solid")
@@ -490,6 +484,7 @@ async def process_file(payload: dict):
 
     except Exception as e:
         raise HTTPException(500, str(e))
+
 
 
 
